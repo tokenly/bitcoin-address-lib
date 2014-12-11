5dbcfe5575507e9b9d25227c0600497becda8941
Devon Weller <dweller@devonweller.com>
2014-12-11 08:52:10 -0600

    moved init to register method

--- src/Tokenly/BitcoinAddressLib/BitcoinAddressServiceProvider.php
+++ src/Tokenly/BitcoinAddressLib/BitcoinAddressServiceProvider.php
@@ -15,7 +15,6 @@ class BitcoinAddressServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->package('tokenly/bitcoin-address-lib', 'bitcoin-address-lib', __DIR__.'/../../');
     }
 
     /**
@@ -25,6 +24,8 @@ class BitcoinAddressServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->package('tokenly/bitcoin-address-lib', 'bitcoin-address-lib', __DIR__.'/../../');
+
         $this->app->bind('Tokenly\BitcoinAddressLib\BitcoinAddressGenerator', function($app) {
             $config = $app['config']['bitcoin-address-lib::bitcoin'];
             $generator = new BitcoinAddressGenerator($config['seed']);
