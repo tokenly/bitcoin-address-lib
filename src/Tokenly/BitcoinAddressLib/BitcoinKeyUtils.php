138aad59b9ec1ce00218aa23197e2e40d925020e
Devon Weller <dweller@devonweller.com>
2015-01-03 09:22:53 -0600

    added more utility methods

--- src/Tokenly/BitcoinAddressLib/BitcoinKeyUtils.php
+++ src/Tokenly/BitcoinAddressLib/BitcoinKeyUtils.php
@@ -2,6 +2,7 @@
 
 namespace Tokenly\BitcoinAddressLib;
 
+use BitWasp\BitcoinLib\BIP32;
 use BitWasp\BitcoinLib\BitcoinLib;
 use Exception;
 
@@ -20,9 +21,17 @@ class BitcoinKeyUtils
         $private_key_details = BitcoinLib::WIF_to_private_key($wif);
         $private_key = $private_key_details['key'];
 
+        return self::publicKeyFromPrivateKey($private_key, $verify_address);
+    }
+
+    public static function publicKeyFromPrivateKey($private_key, $verify_address=null) {
         $address_version = '00';
-        $trial_wif = BitcoinLib::private_key_to_WIF($private_key, true, $address_version);
-        if ($trial_wif !== $wif) { throw new Exception("WIF re-encoding failed", 1); }
+
+        // convert long-form private key
+        if (substr($private_key, 0, 4) == 'xprv') {
+            $key_details = BIP32::import($private_key);
+            $private_key = $key_details['key'];
+        }
 
         $compressed_public_key = BitcoinLib::private_key_to_public_key($private_key, true);
 
@@ -33,6 +42,14 @@ class BitcoinKeyUtils
 
         return $compressed_public_key;
     }
+
+    public static function WIFFromPrivateKey($private_key) {
+        $key_details = BIP32::import($private_key);
+        $wif = BitcoinLib::private_key_to_WIF($key_details['key'], true, $key_details['version']);
+        if (!BitcoinLib::validate_WIF($wif)) { throw new Exception("Failed to validate WIF", 1); }
+        return $wif;
+    }
+
 
     public static function addressFromWIF($wif) {
         $is_valid = BitcoinLib::validate_WIF($wif);
